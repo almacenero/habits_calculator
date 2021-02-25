146bc2ffd7da20f7ea71914a0ecf87b799a56098
Yasser <yassergeo@gmail.com>
2021-02-25 15:40:52 -0500

    refactory operators functions

--- __test__/button_test.test.js
+++ __test__/button_test.test.js
@@ -1,13 +1,40 @@
 const panelButton = require("./../index");
 
 describe("Button should recive  values", () => {
-  test("Button should recive a value number value", () => {
-    let a = "sd";
-    expect(panelButton(a)).toBe(null);
-  });
   test("Button should concatenate input values", () => {
     expect(panelButton(5)).toEqual(5);
     expect(panelButton(1)).toEqual(51);
     expect(panelButton(0)).toEqual(510);
+  });
+});
+
+describe("Button should math  operations", () => {
+  test("Should add 2 numbers", () => {
+    panelButton("C");
+    expect(panelButton(4)).toEqual(4);
+    expect(panelButton(6)).toEqual(46);
+
+    panelButton("+");
+
+    expect(panelButton(10)).toEqual(10);
+    expect(panelButton(1)).toEqual(101);
+
+    panelButton("+");
+
+    expect(panelButton(3)).toEqual(3);
+    expect(panelButton(2)).toEqual(32);
+
+    panelButton("*");
+
+    expect(panelButton(3)).toEqual(3);
+    expect(panelButton(6)).toEqual(36);
+
+    panelButton("/");
+
+    expect(panelButton(2)).toEqual(2);
+
+    panelButton("=");
+
+    panelButton("C");
   });
 });
--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const stringNumber = [];
+let stringNumber = [];
+let stringOperationsNumbers = [];
+
+let actualNumber = 0;
+
+const clearCalculator = () => {
+  stringNumber = [];
+  stringOperationsNumbers = [];
+  total = 0;
+  actualNumber = 0;
+  console.log(stringOperationsNumbers);
+};
+
+const equalValue = () => {
+  stringNumber = [];
+
+  stringOperationsNumbers.push(actualNumber);
+  actualNumber = 0;
+  console.log(stringOperationsNumbers);
+};
+
+const manageOperator = (operator) => {
+  stringNumber = [];
+  stringOperationsNumbers.push(actualNumber);
+  stringOperationsNumbers.push(operator);
+  actualNumber = 0;
+};
 
 const panelButton = (value) => {
-  if (typeof value != "number") {
-    return null;
+  if (value === "C") {
+    clearCalculator();
   }
   if (value === "=") {
+    equalValue();
   }
   if (value === "+") {
+    manageOperator("+");
   }
   if (value === "-") {
+    manageOperator("-");
   }
   if (value === "*") {
+    manageOperator("*");
   }
   if (value === "/") {
+    manageOperator("/");
   }
   if (typeof value == "number") {
     stringNumber.push(value);
-    const number = Number(stringNumber.join(""));
-    //console.log(number);
-    return number;
+    actualNumber = Number(stringNumber.join(""));
+
+    console.log(actualNumber);
+    return actualNumber;
   }
 };
 
